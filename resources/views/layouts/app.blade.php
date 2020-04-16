9e953477c8916aaa015f84d691acbea07903d4a3
Madalitso Nyemba <mnyemba@bintelanalytics.mw>
2020-04-16 19:27:07 +0200

    demo template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,25 +13,25 @@
     <title>Dashboard</title>
 
     <!-- Fontfaces CSS-->
-    <link href="{{asset('css/font-face.css')}}" rel="stylesheet" media="all">
-    <link href="{{asset('vendor/font-awesome-4.7/css/font-awesome.min.css')}}" rel="stylesheet" media="all">
-    <link href="{{asset('vendor/font-awesome-5/css/fontawesome-all.min.css')}}" rel="stylesheet" media="all">
-    <link href="{{asset('vendor/mdi-font/css/material-design-iconic-font.min.css')}}" rel="stylesheet" media="all">
+    <link href="{{asset('assets/css/font-face.css')}}" rel="stylesheet" media="all">
+    <link href="{{asset('assets/vendor/font-awesome-4.7/css/font-awesome.min.css')}}" rel="stylesheet" media="all">
+    <link href="{{asset('assets/vendor/font-awesome-5/css/fontawesome-all.min.css')}}" rel="stylesheet" media="all">
+    <link href="{{asset('assets/vendor/mdi-font/css/material-design-iconic-font.min.css')}}" rel="stylesheet" media="all">
 
     <!-- Bootstrap CSS-->
-    <link href="{{asset('vendor/bootstrap-4.1/bootstrap.min.css')}}" rel="stylesheet" media="all">
+    <link href="{{asset('assets/vendor/bootstrap-4.1/bootstrap.min.css')}}" rel="stylesheet" media="all">
 
     <!-- Vendor CSS-->
-    <link href="{{asset('vendor/animsition/animsition.min.css')}}" rel="stylesheet" media="all">
-    <link href="{{asset('vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css')}}" rel="stylesheet" media="all">
-    <link href="{{asset('vendor/wow/animate.css')}}" rel="stylesheet" media="all">
-    <link href="{{asset('vendor/css-hamburgers/hamburgers.min.css')}}" rel="stylesheet" media="all">
-    <link href="{{asset('vendor/slick/slick.css')}}" rel="stylesheet" media="all">
-    <link href="{{asset('vendor/select2/select2.min.css')}}" rel="stylesheet" media="all">
-    <link href="{{asset('vendor/perfect-scrollbar/perfect-scrollbar.css')}}" rel="stylesheet" media="all">
+    <link href="{{asset('assets/vendor/animsition/animsition.min.css')}}" rel="stylesheet" media="all">
+    <link href="{{asset('assets/vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css')}}" rel="stylesheet" media="all">
+    <link href="{{asset('assets/vendor/wow/animate.css')}}" rel="stylesheet" media="all">
+    <link href="{{asset('assets/vendor/css-hamburgers/hamburgers.min.css')}}" rel="stylesheet" media="all">
+    <link href="{{asset('assets/vendor/slick/slick.css')}}" rel="stylesheet" media="all">
+    <link href="{{asset('assets/vendor/select2/select2.min.css')}}" rel="stylesheet" media="all">
+    <link href="{{asset('assets/vendor/perfect-scrollbar/perfect-scrollbar.css')}}" rel="stylesheet" media="all">
 
     <!-- Main CSS-->
-    <link href="{{asset('css/theme.css')}}" rel="stylesheet" media="all">
+    <link href="{{asset('assets/css/theme.css')}}" rel="stylesheet" media="all">
 
 </head>
 
@@ -450,28 +450,28 @@
     </div>
 
     <!-- Jquery JS-->
-    <script src="{{asset('vendor/jquery-3.2.1.min.js')}}"></script>
+    <script src="{{asset('assets/vendor/jquery-3.2.1.min.js')}}"></script>
     <!-- Bootstrap JS-->
-    <script src="{{asset('vendor/bootstrap-4.1/popper.min.js')}}"></script>
-    <script src="{{asset('vendor/bootstrap-4.1/bootstrap.min.js')}}"></script>
+    <script src="{{asset('assets/vendor/bootstrap-4.1/popper.min.js')}}"></script>
+    <script src="{{asset('assets/vendor/bootstrap-4.1/bootstrap.min.js')}}"></script>
     <!-- Vendor JS       -->
-    <script src="{{asset('vendor/slick/slick.min.js')}}">
+    <script src="{{asset('assets/vendor/slick/slick.min.js')}}">
     </script>
-    <script src="{{asset('vendor/wow/wow.min.js')}}"></script>
-    <script src="{{asset('vendor/animsition/animsition.min.js')}}"></script>
-    <script src="{{asset('vendor/bootstrap-progressbar/bootstrap-progressbar.min.js')}}">
+    <script src="{{asset('assets/vendor/wow/wow.min.js')}}"></script>
+    <script src="{{asset('assets/vendor/animsition/animsition.min.js')}}"></script>
+    <script src="{{asset('assets/vendor/bootstrap-progressbar/bootstrap-progressbar.min.js')}}">
     </script>
-    <script src="{{asset('vendor/counter-up/jquery.waypoints.min.js')}}"></script>
-    <script src="{{asset('vendor/counter-up/jquery.counterup.min.js')}}">
+    <script src="{{asset('assets/vendor/counter-up/jquery.waypoints.min.js')}}"></script>
+    <script src="{{asset('assets/vendor/counter-up/jquery.counterup.min.js')}}">
     </script>
-    <script src="{{asset('vendor/circle-progress/circle-progress.min.js')}}"></script>
-    <script src="{{asset('vendor/perfect-scrollbar/perfect-scrollbar.js')}}"></script>
-    <script src="{{asset('vendor/chartjs/Chart.bundle.min.js')}}"></script>
-    <script src="{{asset('vendor/select2/select2.min.js')}}">
+    <script src="{{asset('assets/vendor/circle-progress/circle-progress.min.js')}}"></script>
+    <script src="{{asset('assets/vendor/perfect-scrollbar/perfect-scrollbar.js')}}"></script>
+    <script src="{{asset('assets/vendor/chartjs/Chart.bundle.min.js')}}"></script>
+    <script src="{{asset('assets/vendor/select2/select2.min.js')}}">
     </script>
 
     <!-- Main JS-->
-    <script src="{{asset('js/main.js')}}"></script>
+    <script src="{{asset('assets/js/main.js')}}"></script>
 
 </body>
 
