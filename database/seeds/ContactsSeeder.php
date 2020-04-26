b73b111deb03adf3747a882755d7da286ece0738
Madalitso Nyemba <mnyemba@bintelanalytics.mw>
2020-04-26 15:36:08 +0200

    added contacts seeder

---
+++ database/seeds/ContactsSeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\ContactTraceUser;
+
+class ContactsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        ContactTraceUser::create([
+            'sender' => 1,
+            'recipient' => 2,
+            'status' => 'pending',
+        ]);
+        ContactTraceUser::create([
+            'sender' => 2,
+            'recipient' => 3,
+            'status' => 'rejected',
+        ]);
+        ContactTraceUser::create([
+            'sender' => 3,
+            'recipient' => 4,
+            'status' => 'accepted',
+        ]);
+        ContactTraceUser::create([
+            'sender' => 1,
+            'recipient' => 3,
+            'status' => 'rejected',
+        ]);
+        ContactTraceUser::create([
+            'sender' => 1,
+            'recipient' => 4,
+            'status' => 'accepted',
+        ]);
+        ContactTraceUser::create([
+            'sender' => 4,
+            'recipient' => 5,
+            'status' => 'pending',
+        ]);
+        ContactTraceUser::create([
+            'sender' => 4,
+            'recipient' => 6,
+            'status' => 'pending',
+        ]);
+
+    }
+}
