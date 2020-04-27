353263c15d2c2f62362fb2a5bebd93b3c02610ca
Madalitso Nyemba <mnyemba@bintelanalytics.mw>
2020-04-28 01:29:07 +0200

    working on add contact

--- app/Http/Controllers/api/UsersController.php
+++ app/Http/Controllers/api/UsersController.php
@@ -28,7 +28,7 @@ class UsersController extends Controller
     }
     public function getUserContacts()
     {
-        $contacts = contactTraceUser::select('id','sender')->where('sender',Auth::user()->id)->orWhere('recipient',Auth::user()->id)->where('status','accepted')->with('User:id,first_name,last_name,phone')->get();
+        $contacts = contactTraceUser::select('id','sender')->where('sender',Auth::user()->id)->where('status','accepted')->with('User:id,first_name,last_name,phone')->get();
         return $contacts;
     }
 
