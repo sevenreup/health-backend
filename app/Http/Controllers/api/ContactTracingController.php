b7b253d7e88e0b85cdd7b83ba6d2faaa047cab18
Madalitso Nyemba <mnyemba@bintelanalytics.mw>
2020-04-30 22:23:55 +0200

    working on add contact

--- app/Http/Controllers/api/ContactTracingController.php
+++ app/Http/Controllers/api/ContactTracingController.php
@@ -22,6 +22,7 @@ class ContactTracingController extends Controller
     }
     public function addContact(request $request,contactTraceUser $contactTraceUser,contactTraceGuest $contactTraceGuest)
     {
+        error_log($request->all());
         for ($i = 0; $i < count($request->all()); $i++) {
 
         $recipientData = $this->User->where('phone',$request->input('recipientNumber')[$i] )->first();
@@ -38,8 +39,8 @@ class ContactTracingController extends Controller
         {
             $data = [];
             $data['sender'] =  Auth::user()->id;
-            $data['recipientName'] =  $request->input('recipientName');
-            $data['recipientNumber'] =  $request->input('recipientNumber');
+            $data['recipientName'] =  $request->input('recipientName')[$i];
+            $data['recipientNumber'] =  $request->input('recipientNumber')[$i];
             $contactTraceGuest->insert($data);
             //send notification to user as well as message
 
