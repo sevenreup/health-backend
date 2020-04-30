385fcd37f80f2907b92bc48cd4de9fcdb3e0d5fa
Madalitso Nyemba <mnyemba@bintelanalytics.mw>
2020-05-01 00:27:06 +0200

    working on add contact

--- app/Http/Controllers/api/ContactTracingController.php
+++ app/Http/Controllers/api/ContactTracingController.php
@@ -22,7 +22,7 @@ class ContactTracingController extends Controller
     }
     public function addContact(request $request,contactTraceUser $contactTraceUser,contactTraceGuest $contactTraceGuest)
     {
-        error_log($request->input('contacts'));
+        error_log($request);
     //     for ($i = 0; $i < count($request->all()); $i++) {
 
     //     $recipientData = $this->User->where('phone',$request->input('recipientNumber')[$i] )->first();
