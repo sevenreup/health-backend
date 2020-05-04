67b102a54357414897ba1e2cc49f8032fba522b0
Madalitso Nyemba <mnyemba@bintelanalytics.mw>
2020-05-05 00:05:50 +0200

    fixed sender and recipient stuff

--- app/contactTraceUserPending.php
+++ app/contactTraceUserPending.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class contactTraceUserPending extends Model
 {
-    //
+    protected $table = "contactTracingUser";
+    public function User()
+    {
+        return $this->belongsTo('App\User', 'sender');
+    }
 }
