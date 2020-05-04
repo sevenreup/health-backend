da6c21f189e362fea9d40ee909a995543f173dcb
Madalitso Nyemba <mnyemba@bintelanalytics.mw>
2020-05-05 01:07:20 +0200

    fixed sender and recipient stuff

--- app/Http/Controllers/api/UsersController.php
+++ app/Http/Controllers/api/UsersController.php
@@ -56,8 +56,8 @@ class UsersController extends Controller
     {
         $contacts = contactTraceUser::select('id','recipient')->where('status','accepted')->where('sender',Auth::user()->id)->orWhere('recipient',Auth::user()->id)->with('User:id,first_name,last_name,phone')->get();
         $contacts2 = contactTraceUser::select('id','recipient')->where('status','accepted')->where('sender',Auth::user()->id)->orWhere('recipient',Auth::user()->id)->with('User:id,first_name,last_name,phone')->get();
-        $finalone = array_merge($contacts,$contacts2);
-        return $finalone;
+        $finalone = array_merge($contacts->toArray(),$contacts2->toArray());
+        return json_encode($finalone);
     }
 
     // for testing
