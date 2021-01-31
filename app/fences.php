751ba53133be78d48507f845f68a1bf35fce136b
Christopher Seven Phiri <chris7phiri@gmail.com>
2021-01-31 16:15:49 +0200

    added fence name and env example

--- app/fences.php
+++ app/fences.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class fences extends Model
 {
+    protected $table ="geofences";
+
     public function events()
     {
         return $this->hasMany('App\events', 'fencesId');
